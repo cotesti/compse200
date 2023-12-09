61dcae435c4f6e997d1a4bd053ebad812a12eb5a
Mikko_A <64309746+Mikelz-dude@users.noreply.github.com>
2023-12-09 15:14:48 +0200

    Fixed the names of tests and expected output
    One test expected 5 instead of NaN, and some tests were not named approperiately

--- toNumber.test.js
+++ toNumber.test.js
@@ -13,20 +13,20 @@ test('does not crash when converting a symbol', () => {
   });
 
 
-test('does not crash when converting an boolean', () => {
+test('does not crash when converting a null', () => {
     expect(toNumber(null)).toBe(0);
   });
 
   var bin = "0b1111";    // bin will be set to 15
   var hex = "+0xF"
   var oct = "0o17"
-  test('does not crash when converting a binary', () => {
+  test('does not crash when converting a octal', () => {
     expect(toNumber(oct)).toBe(15);
   });
   test('does not crash when converting a binary', () => {
     expect(toNumber(bin)).toBe(15);
   });
-  test('does not crash when converting a binary', () => {
+  test('does not crash when converting a hex code', () => {
     expect(toNumber(hex)).toBe(NaN);
   });
 
@@ -35,18 +35,18 @@ test('does not crash when converting an boolean', () => {
     return func
  }
 
-  test('does not crash when converting an object', () => {
-    expect(toNumber(func())).toBe(5);
+  test('does not crash when converting a function', () => {
+    expect(toNumber(func())).toBe(NaN);
   });
   
 test('does not crash when converting an object', () => {
     expect(toNumber({'value': 5 })).toBe(0/0);
   });
 
-  test('does not crash when converting a binary', () => {
+  test('does not crash when converting a number object', () => {
     expect(toNumber(new Number(10))).toBe(10);
   });  
   
-  test('does not crash when converting a binary', () => {
+  test('does not crash when converting a empty number object', () => {
     expect(toNumber(new Number())).toBe(0);
   });
