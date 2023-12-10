dd2b6b8352a35c0c02455523b5cb22d0836ac8f7
prrdee <18732242+prrdee@users.noreply.github.com>
2023-12-10 16:02:17 +0200

    Fix jest config
    - ignore .internal folder from coverage

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,4 +2,8 @@ export default {
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
+  collectCoverageFrom: [
+    "<rootDir>/COMP.SE.200-2023-2024-1-main/**/*.js",
+    "!<rootDir>/COMP.SE.200-2023-2024-1-main/src/.internal/**"
+  ],
 };
