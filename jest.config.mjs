d94ff79ff2dca4d8b04f1e18dcc4c68600635a96
prrdee <18732242+prrdee@users.noreply.github.com>
2023-12-11 19:53:13 +0200

    Add coverageThreshold 95

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,15 +1,15 @@
 const filesUnderTest = [
-  "camelCase.js",
-  "ceil.js",
-  "filter.js",
-  "get.js",
-  "isLength.js",
-  "toNumber.js",
-  "toString.js",
-  "upperFirst.js",
-  "words.js",
-  "isDate.js",
-  "isEmpty.js"
+  'camelCase.js',
+  'ceil.js',
+  'filter.js',
+  'get.js',
+  'isLength.js',
+  'toNumber.js',
+  'toString.js',
+  'upperFirst.js',
+  'words.js',
+  'isDate.js',
+  'isEmpty.js',
 ].map(file => `<rootDir>/COMP.SE.200-2023-2024-1-main/src/${file}`);
 
 export default {
@@ -19,7 +19,14 @@ export default {
   verbose: true,
   collectCoverageFrom: [
     ...filesUnderTest,
-    "!<rootDir>/COMP.SE.200-2023-2024-1-main/src/.internal/**"
+    '!<rootDir>/COMP.SE.200-2023-2024-1-main/src/.internal/**',
   ],
+  coverageThreshold: {
+    global: {
+      statements: 95,
+      branches: 95,
+      functions: 95,
+      lines: 95,
+    },
+  },
 };
-
