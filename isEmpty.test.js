3c7d927ca5e71f0d9a2bc9a26fec8761c30a316f
prrdee <18732242+prrdee@users.noreply.github.com>
2023-12-11 16:40:31 +0200

    Add more tests to fully cover isEmpty function

--- isEmpty.test.js
+++ isEmpty.test.js
@@ -15,11 +15,13 @@ describe('isEmpty', () => {
   test('distinguishes between empty and non-empty arrays', () => {
     expect(isEmpty([])).toBe(true);
     expect(isEmpty([1, 2, 3])).toBe(false);
+    expect(isEmpty(new Uint8Array)).toBe(true)
   });
 
   test('distinguishes between empty and non-empty objects', () => {
     expect(isEmpty({})).toBe(true);
     expect(isEmpty({ a: 1 })).toBe(false);
+    expect(isEmpty(function() { return arguments }())).toBe(true);
   });
 
   test('handles array-like objects correctly', () => {
@@ -37,4 +39,21 @@ describe('isEmpty', () => {
   test('returns true for functions and other non-standard objects', () => {
     expect(isEmpty(function() {})).toBe(true);
   });
+
+  test('distinguishes between empty and non-empty prototype objects', () => {
+    expect(isEmpty(Object.prototype)).toBe(true);
+    const customPrototype = Object.create(Object.prototype);
+    customPrototype.foo = 'bar';
+    expect(isEmpty(customPrototype)).toBe(false);
+  });
+
+  test('should return true for objects with multiple prototype properties', () => {
+    const prototype1 = { foo: 'bar' };
+    const prototype2 = { baz: 'qux' };
+    const object = {};
+    Object.setPrototypeOf(object, prototype1);
+    Object.setPrototypeOf(prototype1, prototype2);
+    expect(isEmpty(object)).toBe(true);
+  });
+  
 });
